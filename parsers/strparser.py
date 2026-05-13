f10d15c2bbeb011810224321060f110b4b5e30e2
snehasish haldar <snehasishhaldar@snehasishs-MacBook-Air.local>
2026-05-13 19:35:12 +0530

    changed

--- parsers/strparser.py
+++ parsers/strparser.py
@@ -12,13 +12,11 @@ model =  ChatGroq(
     model="llama-3.1-8b-instant"
 )
 
-# 1st prompt -> detailed report
 template1 = PromptTemplate(
     template='Write a detailed report on {topic}',
     input_variables=['topic']
 )
 
-# 2nd prompt -> summary
 template2 = PromptTemplate(
     template='Write a 5 line summary on the following text. /n {text}',
     input_variables=['text']
