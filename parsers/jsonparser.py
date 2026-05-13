2c37b8fb807d351c8a424bc232912ed3706701de
snehasish haldar <snehasishhaldar@snehasishs-MacBook-Air.local>
2026-05-13 16:32:57 +0530

    Added new files

---
+++ parsers/jsonparser.py
@@ -0,0 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+model =  ChatGroq(
+    model="llama-3.1-8b-instant"
+)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'Virat Kohli'})
+
+print(result)
